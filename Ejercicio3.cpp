e352a4f7578337ab3686be934930a0f13c38f818
Otoniel2514 <81173407+Otoniel2514@users.noreply.github.com>
2021-05-04 14:43:52 -0700

    Add files via upload

---
+++ Ejercicio3.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+using namespace std;
+int main()
+{
+    cout<<"La cantidad de bytes de tipo char es: "<<sizeof(char)<<endl;
+    cout<<"La cantidad de bytes de tipo int es: "<<sizeof(int)<<endl;
+    cout<<"La cantidad de bytes de tipo long es: "<<sizeof(long)<<endl;
+
+    cout<<"La cantidad de bytes de tipo float es: "<<sizeof(float)<<endl;
+    cout<<"La cantidad de bytes de tipo double es: "<<sizeof(double)<<endl;
+    cout<<"La cantidad de bytes de tipo long double es: "<<sizeof(long double)<<endl;
+    
+     return 0;
+
+}
+
