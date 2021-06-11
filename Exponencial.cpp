5462910be6bffcec416897ac3c7f5a14e5527924
Otoniel2514 <81173407+Otoniel2514@users.noreply.github.com>
2021-06-10 19:47:33 -0700

    UploadLabo

---
+++ Exponencial.cpp
@@ -0,0 +1,57 @@
+//Programa que calcule la aproximacion de e elevado a la x mediante serie
+#include "iostream"
+#include "math.h"
+using namespace std;
+int main()
+{
+    float Exp = 0; 
+    double fact = 1; 
+    int i = 0; // Comienza en cero ya que es una suma sucesiva de valores
+    int x,y,z;
+
+    cout<<"Introduzca el valor de x: "<<endl;
+    cin>>x;
+    cout<<"Introduzca el numero de terminos que desea en la serie: "<<endl;
+    cin>>y;
+
+    while (i <= y)
+    {
+        z = 1;
+        fact = 1; 
+        while(z <= i) 
+        {
+            fact = fact * z;
+            z = z + 1;
+        }
+        Exp = Exp + pow(x,i)/fact; //Formula
+        i = i + 1;
+    }
+    cout<<endl;
+    cout<<"El valor de e elevado a la x es: "<<Exp<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+/*while (i <= y)
+    {
+        z = 1;
+        fact = 1; 
+        while(z <= i) 
+        {
+            fact = fact * z;
+            z = z + 1;
+        }
+        Exp = Exp + pow(x,i)/fact; //Formula
+        i = i + 1;
+    }
+    cout<<endl;
+    cout<<"El valor de e elevado a la x es: "<<Exp<<endl;
+    return 0;
+}*/
