504f8ed5a1d8886bc6ad78a8690c0a0dc03c67df
Otoniel2514 <81173407+Otoniel2514@users.noreply.github.com>
2021-07-02 10:19:01 -0700

    UploadLaboratorio

---
+++ BinarioADecimal.cpp
@@ -0,0 +1,30 @@
+//Programa que Convierta un número binario, a su número decimal equivalente, ingesado desde teclado.
+#include "iostream"
+using namespace std;
+
+void convertirBinarioADecimal(int, int[], int&);
+void imprimirDecimal(int[], int);
+int main()
+{
+    int binario, digitosDecimal;
+    int decimal[100];
+    cout<<"Ingrese el numero Binario que desea convertir a decimal: "<<endl;
+    cin>>binario;
+    convertirBinarioADecimal(binario, decimal, digitosDecimal);
+    cout<<"El numero Decimal resultante de su Binario ingresado es: "<<endl;
+    imprimirDecimal(decimal, digitosDecimal);
+    return 0;
+}
+void convertirBinarioADecimal(int binario, int decimal[], int &digitosDecimal)
+{
+    while (binario > 0)
+    {
+        decimal[digitosDecimal] = binario%2;
+        binario = binario / 2;
+        digitosDecimal++;
+    }
+}
+void imprimirDecimal(int arreglo[], int n){
+    for(int i = (n - 1); i >= 0; i--) cout<<arreglo[i];
+    cout<<endl;
+}
